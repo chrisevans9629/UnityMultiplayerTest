fda9c0c50bb3c55f3ebe1367a7f1528d84e7b300
Chris Evans <chrisevans9629@gmail.com>
2022-10-01 22:43:49 -0500

    added bullets

---
+++ Assets/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Assets
+{
+    public class Bullet : NetworkBehaviour
+    {
+        public float Speed = 4;
+        public Vector3 Direction;
+
+        private void Update()
+        {
+            if (!IsServer)
+                return;
+            transform.position += Direction.normalized * Speed * Time.deltaTime;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (!IsServer)
+                return;
+            if (collision.transform.tag == "Enemy")
+            {
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Weapon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Assets
+{
+    public class Weapon : MonoBehaviour
+    {
+        [SerializeField]
+        private Bullet BulletPrefab;
+
+        [SerializeField]
+        private float Speed = 1;
+
+        [SerializeField]
+        private float BulletSpeed = 4;
+
+        [SerializeField]
+        private float Range = 10;
+
+        private void Start()
+        {
+            StartCoroutine(Fire());
+        }
+
+        IEnumerator Fire()
+        {
+            yield return new WaitForSeconds(Speed);
+            var enemy = GameObject.FindGameObjectsWithTag("Enemy")
+                .Where(p => Vector3.Distance(p.transform.position, transform.position) <= Range)
+                .FirstOrDefault();
+
+            if (enemy != null)
+            {
+                var item = Instantiate(BulletPrefab);
+                item.Direction = enemy.transform.position - transform.position;
+                item.Speed = BulletSpeed;
+                item.transform.position = transform.position;
+                item.GetComponent<NetworkObject>().Spawn(true);
+            }
+
+            StartCoroutine(Fire());
+        }
+    }
+}
